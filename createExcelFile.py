cf3b5a596831966ba14ab33f947500d142d41155
Scott Silliman <ssilliman@pitt.edu>
2020-01-23 06:44:06 -0500

    now fully automated. kruft removed

--- createExcelFile.py
+++ createExcelFile.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from io import StringIO
 from copy import copy
 import csv
+import glob
 import json
 import os
 import re
@@ -11,7 +12,8 @@ import xlsxwriter
 
 TUTOR_STATE_FILE_NAME_RE = re.compile(r'^(\w+)\.tutor-state.json$')
 SSCENE1_FILE_NAME_RE = re.compile(r'^(\w+)\.SScene1_1.json$')
-FILE_NAME_RE = re.compile(r^(\w+))\.(?:tutor-state|SScene1_1)\.json$')
+FILE_NAME_RE = re.compile(r'^\w+?\.(?:tutor-state|SScene1_1)\.json$')
+ANALYZED_CSV_FILE_RE = re.compile(r'^(\w+?)\.step3\.analyzed\.csv$')
 
 
 
@@ -62,7 +64,6 @@ def filter_recs3(recs):
 
 
 def read_csv(file_name):
-    # reader = None
     rows = []
     with open(file_name, "r") as infh:
         reader = csv.DictReader(infh)
@@ -70,61 +71,8 @@ def read_csv(file_name):
             rows.append((i+2, dict(row)))
     return rows
 
-# def dump_json(obj, path):
-#     with open(path, 'w') as fh:
-#         json.dump(obj, fh, indent=4)
-
-# def dump_simplified_json(scene1_seq, module_seq, path):
-#     print(path)
-#     indent = ""
-#     txt = "{\n"
-#     indent = 4 * " "
-#     txt += '%s"scene.SScene1.$seq": [\n' % indent
-#     indent = 8 * " "
-#     for rec in scene1_seq:
-#         txt += "%s%s,\n" % (indent, json.dumps(rec))
-#     txt = txt.rstrip(',\n')
-#     txt += '\n'
-#     indent = 4 * " "
-#     txt += "%s],\n" % indent
-#     txt += '%s"module.EFMod_TEDInstr.$seq": [\n' % indent
-#     indent = 8 * " "
-#     for rec in module_seq:
-#         value = rec.get('value')
-#         if isinstance(value, (dict, list)):
-#             val = json.dumps(value, indent=4)
-#             lines = val.rstrip().split('\n')
-#             nested_indent = "    " + indent
-#             txt += '%s{\n' % indent
-#             txt += '%s"prop": "%s",\n' % (nested_indent, rec.get('prop'))
-#             txt += '%s"time": %d,\n' % (nested_indent, rec.get('time'))
-#             if isinstance(value, (dict)):
-#                 txt += '%s"value": {\n' % nested_indent
-#             else:
-#                 txt += '%s"value": [\n' % nested_indent
-#             for line in lines[1:-1]:
-#                 txt += "%s%s\n" % (nested_indent, line)
-#             txt = txt.rstrip(',\n')
-#             txt += '\n'
-#             if isinstance(value, (dict)):
-#                 txt += '%s}\n' % nested_indent
-#             else:
-#                 txt += '%s]\n' % nested_indent
-#             txt += "%s},\n" % indent
-#         else:
-#             txt += "%s%s,\n" % (indent, json.dumps(rec))
-#     txt = txt.rstrip(',\n')
-#     txt += '\n'
-#     indent = 4 * " "
-#     txt += "%s]\n}\n" % indent
-#     # print(txt)
-#     sanity_check = json.loads(txt)
-#     with open(path, "w") as fh:
-#         fh.write(txt)
-
 
 def dump_csv(obj, path):
-    # print("writing %s" % path)
     field_names = ['time', 'prop', 'value']
     with open(path, "w") as fh:
         writer = csv.DictWriter(fh, fieldnames=field_names, quoting=csv.QUOTE_NONNUMERIC)
@@ -134,7 +82,6 @@ def dump_csv(obj, path):
 
 
 def dump_simplified_csv(recs, path):
-    # print("writing %s" % path)
     field_names = ['time', 'prop', 'value']
     filtered1 = filter_recs1(recs)
     filtered2 = filter_recs2(filtered1)
@@ -229,65 +176,12 @@ def analyze_simplified_csv(orig, new_file):
                 writer.writerow(rec)
    
 
-# def merge_seqs(scene1_seqs, module_seqs, file_name):
-#     print(file_name)
-#     records = []
-#     for i, rec in enumerate(scene1_seqs):
-#         obj = {
-#             "time": rec.pop('time'),
-#             "path": "scene.SScene1.$seq[%03d]" % i,
-#             "data": json.dumps(rec)
-#         }
-#         records.append(obj)
-#     for i, rec in enumerate(module_seqs):
-#         obj = {
-#             "time": rec.pop('time'),
-#             "path": "module.EFMod_TEDInstr.$seq[%03d]" % i,
-#             "data": json.dumps(rec)
-#         }
-#         records.append(obj)
-#     records.sort(key=lambda x: x['path'])
-#     records.sort(key=lambda x: x['time'])
-#     fields = ["time", "path", "data"]
-#     with open(file_name, "w") as outfh:
-#         writer = csv.DictWriter(outfh, fieldnames=fields, quoting=csv.QUOTE_NONNUMERIC)
-#         writer.writeheader()
-#         for rec in records:
-#             writer.writerow(rec)
-
-SSCENE1_SEQ_CSV_RE = re.compile(r'^(\w+)\.SScene1_seq\.csv$')
-SSCENE1_SEQ_ANALYZED_RE = re.compile(r'^(\w+)\.SScene1_seq\.simplified_csv\.analyzed\.csv$')
-
-
-# def mk_scene1_excel_file(path, csv_files):
-#     # print(csv_files)
-#     wb = xlsxwriter.Workbook(os.path.join(path,'SScene1_seqs.xlsx'))
-#     bold = wb.add_format({'bold': True})
-#     ws = wb.add_worksheet()
-#     # write header
-#     ws.write('A1', 'Student')
-#     ws.write('B1', 'Time', bold)
-#     ws.write('C1', 'Prop', bold)
-#     ws.write('D1', 'Value', bold)
-#     start_row = 0
-#     for csv_file in sorted(csv_files):
-#         mat = SSCENE1_SEQ_CSV_RE.match(csv_file)
-#         if mat is None:
-#             raise Exception("no match %s" % csv_file)
-#         student = mat.group(1)
-#         ws_data = read_csv(os.path.join(path, csv_file))
-#         for (row_num, row_data) in ws_data:
-#             ws.write('A%d' % (row_num + start_row), student)
-#             ws.write('B%d' % (row_num + start_row), row_data['time'])
-#             ws.write('C%d' % (row_num + start_row), row_data['prop'])
-#             ws.write('D%d' % (row_num + start_row), row_data['value'])
-#         start_row += len(ws_data) + 1
-#     wb.close()
-
-
-def mk_analyzed_seq_excel_file(path, csv_files):
-    # print(csv_files)
-    wb = xlsxwriter.Workbook(os.path.join(path, "SScene1_seqs_analysis.xlsx"))
+def create_excel_file(path, analyzed_file_pattern):
+    outfile = path.replace("_logs", ".xlsx")
+    output_path = os.path.join(path, outfile)
+    analyzed_file_pattern = analyzed_file_pattern.replace("%s", "*")
+    csv_files = glob.glob(analyzed_file_pattern)
+    wb = xlsxwriter.Workbook(output_path)
     bold = wb.add_format({'bold': True})
     ws = wb.add_worksheet()
     # write header
@@ -300,11 +194,13 @@ def mk_analyzed_seq_excel_file(path, csv_files):
     ws.write('G1', 'isCorrect', bold)
     start_row = 0
     for csv_file in sorted(csv_files):
-        mat = SSCENE1_SEQ_ANALYZED_RE.match(csv_file)
+        bn = os.path.basename(csv_file)
+        # this SHOUlD match since it already matched the glob
+        mat = ANALYZED_CSV_FILE_RE.match(bn)
         if mat is None:
             raise Exception("no match %s" % csv_file)
         student = mat.group(1)
-        ws_data = read_csv(os.path.join(path, csv_file))
+        ws_data = read_csv(csv_file)
         for (row_num, row_data) in ws_data:
             ws.write('A%d' % (row_num + start_row), student)
             ws.write('B%d' % (row_num + start_row), row_data['variable'])
@@ -315,62 +211,99 @@ def mk_analyzed_seq_excel_file(path, csv_files):
             ws.write('G%d' % (row_num + start_row), row_data['isCorrect'])
         start_row += len(ws_data) + 1
     wb.close()
-
-def mk_excel_files(path, file_names):
-    # scene1_seq_csv_files = []
-    scene1_seq_analyzed_csv_files = []
-    for file_name in file_names:
-        # print(file_name)
-        # if SSCENE1_SEQ_CSV_RE.match(file_name):
-        #     scene1_seq_csv_files.append(file_name)
-        if SSCENE1_SEQ_ANALYZED_RE.match(file_name):
-            scene1_seq_analyzed_csv_files.append(file_name)
-    # print(scene1_seq_csv_files)
-    # print(scene1_seq_analyzed_csv_files)
-    # mk_scene1_excel_file(path, scene1_seq_csv_files)
-    mk_analyzed_seq_excel_file(path, scene1_seq_analyzed_csv_files)
+    print("excel file saved at %s" % output_path)
 
 
-def process_file(path, base_name):
-    tutor_state_file_name = "%s.tutor_state.json" % base_name
-    sscene1_file_name = "%s.SScene1_1.json" % base_name
-
-    print(orig_file_name)
+def read_log_file(student_data, path, orig_file_name):
+    file_type = False
+    tutor_state_mat = TUTOR_STATE_FILE_NAME_RE.match(orig_file_name)
+    sscene1_mat = SSCENE1_FILE_NAME_RE.match(orig_file_name)
+    bn = None # (username) used as basename in output file names
+    if tutor_state_mat:
+        file_type = "tutor_file"
+        bn = tutor_state_mat.group(1)
+    elif sscene1_mat:
+        file_type = "sscene1_file"
+        bn = sscene1_mat.group(1)
+    if not file_type:
+        print("ERROR: '%s' is neither a tutor-state nor sscene1 file")
+        return
+    
+    #too hard to make a default dict this deep
+    if bn not in student_data:
+        student_data[bn] = {}
 
     orig_file_path = os.path.join(path, orig_file_name)
     obj = json.load(open(orig_file_path, "r"))
-
-    sceneKey1 = 'scene'
-    sceneKey2 = 'sceneState'
-    sn = 'SScene1_seq'
+    sceneKey = 'scene'
+    if sceneKey not in obj:
+        sceneKey = 'sceneState'
+        if sceneKey not in obj:
+            print("ERRORL no scene data in %s" % orig_file_name)
+            return
 
     sscene1_seq = obj.get(sceneKey, {}).get('SScene1', {}).get('$seq', None)
-    if sscene1_seq is None:
-        raise Exception('Error no SScene1.$seq in %s' % orig_file_name)
-    # json_file_path = os.path.join(path, "%s.%s.json" % (base_name, sn))
-    csv_file_path = os.path.join(path, "%s.%s.csv" % (base_name, sn))
-    csv_simplified_path = os.path.join(path, "%s.%s.simplified.csv" % (base_name, sn))
-    csv_simplified_analyzed_path = os.path.join(path, "%s.%s.simplified_csv.analyzed.csv" % (base_name, sn))
-    print(csv_file_path)
-    print(csv_simplified_path)
-    print(csv_simplified_analyzed_path)
-    # dump_json(sscene1_seq, json_file_path)
-    # dump_csv(sscene1_seq, csv_file_path)
-    # dump_csv_simplified(sscene1_seq, csv_simplified_path)
-    # analyze_simplified_csv(csv_simplified_path, csv_simplified_analyzed_path)
-    # dump_simplified_json(sscene1_seq, module_seq, simplified_json_path)
-    # print(only_seq_file_path)
+    # may store 'None'. this will be handled by process_data()
+    student_data[bn][file_type] = sscene1_seq
+    
+
+def process_data(orig_data, new_data, path):
+    missing_data_users = []
+    for user_name in orig_data:
+        user_data = orig_data[user_name]
+        have_tutor_data, have_scene_data = False, False
+        if "tutor_file" in user_data:
+            if user_data["tutor_file"] is not None:
+                have_tutor_data = True
+        if "scene1_file" in user_data:
+            if user_data["scene1_file"] is not None:
+                have_scene_data = True
+        if not (have_tutor_data or have_scene_data):
+            missing_data_users.append(user_name)
+        elif have_tutor_data:
+            new_data[user_name] = user_data["tutor_file"]
+        elif have_scene_data:
+            new_data[user_name] = user_data["scene1_file"]
+        else:
+            print("ERROR: How the *^%# did I get here!")
+    return missing_data_users
+
+
+def dump_intermediate_files(data, step1_path, step2_path, step3_path):
+    for user_name in data:
+        sscene1_seq = data[user_name]
+        step1_file = step1_path % user_name
+        step2_file = step2_path % user_name
+        step3_file = step3_path % user_name
+        dump_csv(sscene1_seq, step1_file)
+        dump_simplified_csv(sscene1_seq, step2_file)
+        analyze_simplified_csv(step2_file, step3_file)
+
 
 def main(path):
+    user_data = {}
+    processed_data = {}
+    int_files_path = os.path.join(path, "intermediate_files")
+    if not os.path.exists(int_files_path):
+        os.makedirs(int_files_path)
+    s1_path = os.path.join(int_files_path, "%s.step1.json2csv.csv")
+    s2_path = os.path.join(int_files_path, "%s.step2.filtered.csv")
+    s3_path = os.path.join(int_files_path, "%s.step3.analyzed.csv")
+
     for (_unused1, _unused2, file_names) in os.walk(path):
         for file_name in sorted(file_names):
             mat = FILE_NAME_RE.match(file_name)
             if mat:
-                base_name = mat.group(1)
-                print(base_name)
-                # process_file(path, base_name)
-                # break
-        # mk_excel_files(path, file_names)          
+                read_log_file(user_data, path, file_name)
+        missing_data_users = process_data(user_data, processed_data, path)
+        dump_intermediate_files(processed_data, s1_path, s2_path, s3_path)
+        create_excel_file(path, s3_path)
+        if len(missing_data_users):
+            error_file = os.path.join(path, "students_missing_data.txt")
+            with open(error_file, "w") as fh:
+                fh.writelines("\n".join(missing_data_users))
+            print("NOTE: A list of usernames missing the required data have "
+                  "been saved to %s" % error_file)
         break
 
 
